403e83f6ba02e830c16098544e43291dccd68dbc
Shubham <shubhamsankpal0302@gmail.com>
2022-09-21 12:31:56 +0530

    This is basic digital Clock using tkinter

---
+++ Clock/clock.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+
+def time():
+    string = strftime("%H:%M:%S  %p")
+    label.config(text=string)
+    label.after(1000, time)
+
+
+
+label = Label(root, font=("ds-digital", 100) ,background="black", foreground ="cyan" )
+label.pack(anchor="center")
+
+time()
+
+
+root.mainloop()
